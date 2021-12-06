8efea96f8aae7fee37e10b6ddb378eb9e9ec9417
Maxx King <80922638+maxx-king@users.noreply.github.com>
2021-12-06 13:00:02 -0800

    Update data.js
    remove numbering from projects

--- src/Components/InfoSection/data.js
+++ src/Components/InfoSection/data.js
@@ -15,7 +15,7 @@ export const Project1 = {
     id: 'IchthysSurfboards',
     img: img2,
     imgStart: true,
-    heading: 'Ichthys Surfboards || 01',
+    heading: 'Ichthys Surfboards',
     subtitle: 'Surfboard manufactuing company based out of San Diego, CA. Utilizing additive manufacturing technologies, composite materials, and machine learning; along with full web3 integration. Pushing the boundries to whats possible in the surfing industry.',
 }
 
@@ -23,7 +23,7 @@ export const Project2 = {
     id: '3DPrinting',
     img: img3,
     imgStart: false,
-    heading: '02 || 3D Printing on Demand',
+    heading: '3D Printing on Demand',
     subtitle: 'Connecting with individuals and businesses in the local community, providing 3D modeling and printing services at competitve prices. Design, Iterate, and Test products to meet client specifications before deadlines.',
 }
 
@@ -31,7 +31,7 @@ export const Project3 = {
     id: 'DolphinID',
     img: img2,
     imgStart: true,
-    heading: 'Dolphin Classification W/ Nerual Network || 03',
+    heading: 'Dolphin Classification W/ Nerual Network',
     subtitle: 'University assignment completed for Professor Marie Roch. Using the data and research provided, was able to create a neural network to identify specied of dolphins based off of their echolocation clicks. Network has over 98% accuracy in its predictions.',
 }
 
@@ -39,6 +39,6 @@ export const Project4 = {
     id: 'PrettyColors',
     img: img4,
     imgStart: false,
-    heading: '04 || Pretty Colors ',
+    heading: 'Pretty Colors ',
     subtitle: 'Python utility to facilitate web design. Tool takes two images and creates a color gradient of equal width by any specified height. Similar to the CSS linear-gradient function, but creates a vertical color gradient for every pixel in the width.',
 }
