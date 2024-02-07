2fbee9ac9b519c4a69b4dbbbf62170c8efa4a315
Maxx King <80922638+maxx-king@users.noreply.github.com>
2024-02-07 12:15:05 -0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,7 @@
-import { Component, ElementRef, NgZone, OnInit, OnDestroy, ViewChild, AfterViewInit } from '@angular/core';
+import { Component, ElementRef, NgZone, ViewChild, AfterViewInit } from '@angular/core';
 import * as THREE from 'three';
-import { STLLoader } from 'three/examples/jsm/loaders/STLLoader'
+import { ThreeMFLoader } from 'three/examples/jsm/loaders/3MfLoader'
+import { OrbitControls } from 'three/examples/jsm/controls/OrbitControls';
 
 @Component({
   selector: 'app-root',
@@ -13,151 +14,111 @@ export class AppComponent implements AfterViewInit {
   private renderer!: THREE.WebGLRenderer;
   private scene!: THREE.Scene;
   private camera!: THREE.PerspectiveCamera;
-  private model!: THREE.Object3D;
+  private controls!: OrbitControls;
+
 
   constructor(private ngZone: NgZone, private el: ElementRef) {}
 
   ngAfterViewInit(): void {
     this.initThree();
     this.addModel();
+    this.initControls();
+    this.addFloor();
     this.animate();
-    this.handleMouseMove();
+
+    window.addEventListener('mousedown', (e) => this.enableControls(e));
+    window.addEventListener('mouseup', (e) => this.disableControls(e));
+  }
+
+  private enableControls(e: MouseEvent): void {
+    if (e.button === 0) {
+      this.controls.enabled = true;
+    }
+  }
+  
+  private disableControls(e: MouseEvent): void {
+    if (e.button === 0) {
+      this.controls.enabled = false;
+    }
   }
 
   ngOnDestroy(): void {
-    // Clean up resources (e.g., event listeners)
+    // TODO: Clean up resources (e.g., event listeners)
   }
 
   private initThree(): void {
     const width = this.rendererContainer.nativeElement.clientWidth;
     const height = this.rendererContainer.nativeElement.clientHeight;
 
-    // Renderer
     this.renderer = new THREE.WebGLRenderer();
     this.renderer.setSize(width, height);
     this.rendererContainer.nativeElement.appendChild(this.renderer.domElement);
     
-
-    // Scene
     this.scene = new THREE.Scene();
+    this.scene.background = new THREE.Color(0x070B06)
 
-    // Camera
     this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
     this.camera.position.z = 25;
 
     // Lighting
-    // Add a directional light facing the object from the right
+    // right
     const directionalLightRight = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLightRight.position.set(1, 0, 0).normalize(); // Adjust position as needed
+    directionalLightRight.position.set(1, 0, 0).normalize(); 
     this.scene.add(directionalLightRight);
-    // Add a directional light facing the object from the left
+    // left
     const directionalLightLeft = new THREE.DirectionalLight(0xffffff, 1);
-    directionalLightLeft.position.set(-1, 0, 0).normalize(); // Adjust position as needed
+    directionalLightLeft.position.set(-1, 0, 0).normalize(); 
     this.scene.add(directionalLightLeft);
-    // Add a top-down directional light
+    // top-down 
     const topDownLight = new THREE.DirectionalLight(0xffffff, 1);
-    topDownLight.position.set(0, 1, 0).normalize(); // Adjust position as needed
+    topDownLight.position.set(0, 1, 0).normalize(); 
     this.scene.add(topDownLight);
-    // Add a bottom-up directional light
+    // bottom-up
     const bottomUpLight = new THREE.DirectionalLight(0xffffff, 1);
-    bottomUpLight.position.set(0, -1, 0).normalize(); // Adjust position as needed
+    bottomUpLight.position.set(0, -1, 0).normalize(); 
     this.scene.add(bottomUpLight);
   }
 
+  private addFloor(): void {
+    const floorGeometry = new THREE.PlaneGeometry(100, 100);
+    const floorMaterial = new THREE.MeshStandardMaterial({ color: 0x000000, side: THREE.DoubleSide, transparent: true, opacity: 0.5});
+    const floor = new THREE.Mesh(floorGeometry, floorMaterial);
+    floor.rotation.x = -Math.PI / 2; // Rotate the floor to be horizontal
+    floor.position.y = -12; // Adjust the height of the floor below the model
+    this.scene.add(floor);
+  }
+
   private addModel(): void {
-    const loader = new STLLoader();
-    loader.load('../../assets/MK_Coin_Logov20 - Copy.stl', (geometry: any) => {
-      const material = new THREE.MeshStandardMaterial({
-        color: 0xC0C0C0C0,
-        roughness: 0.1, 
-        metalness: .9,
-        // emissive: 0xC0C0C0C0, // Emissive color, adjust as needed
-        // emissiveIntensity: .2, // Emissive intensity, adjust as needed
-        // envMapIntensity: .2, // Adjust as needed
-      });
-      this.model = new THREE.Mesh(geometry, material);
-
-      // Scale the model appropriately
-      this.model.scale.set(0.1, 0.1, 0.1);
-
-      // Center the model in the scene
-      const box = new THREE.Box3().setFromObject(this.model);
-      const center = box.getCenter(new THREE.Vector3());
-      this.model.position.sub(center);
-
-      this.scene.add(this.model);
+    const loader = new ThreeMFLoader();
+    loader.load('../../assets/mk_coin_logo.3mf', (obj) => { 
+      obj.scale.set(0.18, 0.18, 0.18);
+      obj.name = "coin"
+      this.scene.add(obj);
     });
   }
 
   private animate(): void {
     this.ngZone.runOutsideAngular(() => {
-      const animateFn = () => {
-        this.updateModelOrientation();
-        this.renderer.render(this.scene, this.camera);
-        requestAnimationFrame(animateFn);
-      };
+      const model = this.scene.getObjectByName('coin');
 
-      animateFn();
-    });
-  }
-
-  private handleMouseMove(): void {
-    let isMouseDown = false;
-
-    window.addEventListener('mousedown', (event) => {
-      isMouseDown = true;
-
-      if (this.model && this.isMouseInsideContainer(event)) {
-        this.model.scale.multiplyScalar(0.92);
+      if (model && !this.controls.enabled) {
+        model.rotation.y += 0.01; 
       }
-    });
-
-    window.addEventListener('mouseup', (event) => {
-      isMouseDown = false;
-
-      if (this.model && this.isMouseInsideContainer(event)) {
-        this.model.scale.multiplyScalar(1.08);
-      }
-    });
-    
-    window.addEventListener('mousemove', (event) => {
-      // Update model orientation based on cursor position
-      const mouseX = (event.clientX / window.innerWidth) * 2 - 1;
-      const mouseY = -(event.clientY / window.innerHeight) * 2 + 1;
-
-      if (this.model && this.isMouseInsideContainer(event)) {
-        // Flip up-down controls
-        this.model.rotation.x = -mouseY;
-        this.model.rotation.y = mouseX;
-      }
-    });
-
-    window.addEventListener('click', () => {
-      // Trigger the stream of 1's and 0's
-      this.shootBinaryStream();
-    });
-  }
-
-  private isMouseInsideContainer(event: MouseEvent): boolean {
-    const rendererContainer = document.getElementById('rendererContainer');
-    
-    if (!rendererContainer) return false;
   
-    const rect = rendererContainer.getBoundingClientRect();
-
-    return (
-      event.clientX >= rect.left &&
-      event.clientX <= rect.right &&
-      event.clientY >= rect.top &&
-      event.clientY <= rect.bottom
-    );
+      // Render scene
+      this.renderer.render(this.scene, this.camera);
+  
+      // Call animate function recursively
+      requestAnimationFrame(() => this.animate());
+    });
   }
 
-  private shootBinaryStream(): void {
-    // Implement the logic to shoot 1's and 0's from the center to the edges
-  }
-
-  private updateModelOrientation(): void {
-    // Update model orientation based on any dynamic factors
+  private initControls(): void {
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enableDamping = true; // Smooth rotation
+    this.controls.dampingFactor = 0.25;
+    this.controls.rotateSpeed = 0.35;
+    this.controls.enableZoom = false; // Disable mousewheel zoom
+    this.controls.enabled = false;
   }
 }
