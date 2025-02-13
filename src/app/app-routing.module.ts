38b74cc54b08e9cee2c9ae9c637af88d6dac0265
Maxx King <80922638+maxx-king@users.noreply.github.com>
2025-02-13 09:33:24 -0800

    wip

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,13 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: '**', component: NotFoundComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,23 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { ThreeJSComponent } from './components/three-js/three-js.component';
+import { HomeComponent } from './pages/home/home.component';
+import { NotFoundComponent } from './pages/not-found/not-found.component';
+import { HeaderComponent } from './components/header/header.component';
+import { CarouselComponent } from './components/carousel/carousel.component';
+import { ResumeComponent } from './pages/resume/resume.component';
+import { ToolsComponent } from './pages/tools/tools.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ThreeJSComponent
+    ThreeJSComponent,
+    HomeComponent,
+    NotFoundComponent,
+    HeaderComponent,
+    CarouselComponent,
+    ResumeComponent,
+    ToolsComponent
   ],
   imports: [
     BrowserModule,
