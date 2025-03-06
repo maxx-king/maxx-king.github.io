5d725e6c4ae46b6559007f67df1e360b70b013cb
Maxx King <80922638+maxx-king@users.noreply.github.com>
2025-03-05 22:15:59 -0800

    wip :poop: :sparkles: :fire:

--- src/app/components/three-js/three-js.component.ts
+++ src/app/components/three-js/three-js.component.ts
@@ -15,8 +15,9 @@ export class ThreeJSComponent implements AfterViewInit, OnDestroy {
   private renderer!: THREE.WebGLRenderer;
   private scene!: THREE.Scene;
   private camera!: THREE.PerspectiveCamera;
-  // private controls!: OrbitControls;
+  private controls!: OrbitControls;
   private resizeObserver!: ResizeObserver;
+  private isInteracting = false;
 
   constructor(private ngZone: NgZone, private el: ElementRef) {}
 
@@ -25,11 +26,14 @@ export class ThreeJSComponent implements AfterViewInit, OnDestroy {
     this.setupResizeHandler();
   }
 
+  /**
+   * Clean up Three.js resources when the component is destroyed
+   */
   ngOnDestroy(): void {
     if (this.resizeObserver) {
       this.resizeObserver.disconnect();
     }
-    // Clean up Three.js resources
+
     this.renderer?.dispose();
     this.scene?.clear();
   }
@@ -46,15 +50,19 @@ export class ThreeJSComponent implements AfterViewInit, OnDestroy {
     this.scene.background = new THREE.Color(0x070B06)
 
     this.camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 1000);
-    this.camera.position.z = 250; // zoom out
+    this.camera.position.z = 250;
 
     this._addFloor();
     this._addModel();
     this._setLights()
+    this.setupControls();
     this._animate();
   }
   
 
+  /**
+   * Adds the lights to the scene
+   */
   private _setLights(): void {
     const directionalLightRight = new THREE.DirectionalLight(0xffffff, 1);
     directionalLightRight.position.set(1, 0, 0).normalize(); 
@@ -67,34 +75,47 @@ export class ThreeJSComponent implements AfterViewInit, OnDestroy {
     this.scene.add(bottomUpLight, topDownLight, directionalLightRight, directionalLightLeft,);
   }
 
+  /**
+   * Adds the floor to the scene
+   */
   private _addFloor(): void {
     const floorGeometry = new THREE.PlaneGeometry(1000, 1000, 200, 200);
     const floorMaterial = new THREE.MeshStandardMaterial({ color: 0x000000, side: THREE.DoubleSide, transparent: true, opacity: 0.5, wireframe: true});
     const floor = new THREE.Mesh(floorGeometry, floorMaterial);
-    floor.rotation.x = -Math.PI / 2; // Rotate the floor to be horizontal
-    floor.position.y = -75; // Adjust the height of the floor below the model
+    floor.rotation.x = -Math.PI / 2;
+    floor.position.y = -75;
     this.scene.add(floor);
   }
 
+  /**
+   * Adds the model to the scene
+   */
   private _addModel(): void {
     const loader = new ThreeMFLoader();
     loader.load('../../assets/mk_coin_logo.3mf', (obj) => { 
+      obj.name = 'model';
       this.scene.add(obj);
     });
   }
 
+  /**
+   * Animates the model
+   */
   private _animate(): void {
     this.ngZone.runOutsideAngular(() => {
-      const model = this.scene.getObjectByName('coin');
-      if (model) {
+      const model = this.scene.getObjectByName('model');
+      if (model && !this.isInteracting) {
         model.rotation.y += 0.01; 
       }
-      // Render scene
+
       this.renderer.render(this.scene, this.camera);
       requestAnimationFrame(() => this._animate());
     });
     }
 
+  /**
+   * Sets up a resize observer to handle window resize events
+   */
   private setupResizeHandler(): void {
     this.resizeObserver = new ResizeObserver(() => {
       this.onResize();
@@ -102,6 +123,9 @@ export class ThreeJSComponent implements AfterViewInit, OnDestroy {
     this.resizeObserver.observe(this.htmlContainer.nativeElement);
   }
 
+  /**
+   * Resizes the renderer and camera when the window is resized
+   */
   private onResize(): void {
     if (!this.renderer || !this.camera) return;
 
@@ -111,5 +135,17 @@ export class ThreeJSComponent implements AfterViewInit, OnDestroy {
     this.camera.aspect = width / height;
     this.camera.updateProjectionMatrix();
     this.renderer.setSize(width, height);
+  }
+
+  private setupControls(): void {
+    this.controls = new OrbitControls(this.camera, this.renderer.domElement);
+    this.controls.enableDamping = true;
+    this.controls.dampingFactor = 0.05;
+    this.controls.addEventListener('start', () => {
+      this.isInteracting = true;
+    });
+    this.controls.addEventListener('end', () => {
+      this.isInteracting = false;
+    });
   }
 }
